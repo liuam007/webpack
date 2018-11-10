75640826c12ebf79bb23df1c391d12476c1fda4b
biu-biu <liuam007@users.noreply.github.com>
2018-11-10 14:40:16 +0800

    Add files via upload

---
+++ app/Greeter.js
@@ -0,0 +1,7 @@
+// Greeter.js
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!2222";
+    return greet;
+  };
+  
---
+++ app/main.js
@@ -0,0 +1,4 @@
+//main.js 
+const greeter = require('./Greeter.js');
+const alerts = require('./alert.js');
+document.querySelector("#root").appendChild(greeter());
